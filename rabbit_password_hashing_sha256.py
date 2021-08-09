545b81682029a1fda4a7c3e6ca5d0721b02365d0
Sercan Ezelhan <48968977+sercanezelhan@users.noreply.github.com>
2021-08-09 14:29:44 +0300

    Update rabbit_password_hashing_sha256.py

--- rabbit_password_hashing_sha256.py
+++ rabbit_password_hashing_sha256.py
@@ -5,15 +5,9 @@ import hashlib
 import struct
 import getpass
 
-password1 = getpass.getpass("password: ")
-password2 = getpass.getpass("again: ")
-
-if password1 != password2:
-  print("passwords do not match")
-  exit(1)
-
+password = getpass.getpass("password: ")
 salt = os.urandom(4)
-tmp0 = salt + password1.encode('utf-8')
+tmp0 = salt + password.encode('utf-8')
 tmp1 = hashlib.sha256(tmp0).digest()
 salted_hash = salt + tmp1
 pass_hash = base64.b64encode(salted_hash)
